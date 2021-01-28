1ae9b9ef03b9b1ec97099822b16bb69743beaa6c
Royer G <royjuni3431@gmail.com>
2021-01-28 16:33:06 -0500

    feat: add welcome message

--- bot.py
+++ bot.py
@@ -8,15 +8,15 @@ if token is not None:
     bot = telebot.TeleBot(token, parse_mode=None)
 
 
-    @bot.message_handler(func=lambda m: True, content_types=['new_chat_participant'])
+    @bot.message_handler(func=lambda message: True, content_types=['new_chat_members'])
     def on_user_joins(message):
-        user = message.from_user
+        user = message.new_chat_member
         if hasattr(user, 'first_name') and user.first_name is not None:
             name = user.first_name
             if hasattr(user, 'last_name') and user.last_name is not None:
                 name = f'{name} {user.last_name}'
         else:
-            name = 'Rockstar'
+            name = 'Hacker'
 
         bot.reply_to(message, messages['welcome'].format(name=name))
 
