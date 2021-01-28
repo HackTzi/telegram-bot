40a0dade91c10ff17506fd4592cbb0734d394a1c
Royer G <royjuni3431@gmail.com>
2021-01-28 15:08:25 -0500

    feat: base bot inclide welcome message, rules, proyects and donations0

--- bot.py
+++ bot.py
@@ -1,23 +1,43 @@
 import os
 import telebot
-
+from message import messages
 
 token = os.environ.get('token', None)
 
 if token is not None:
     bot = telebot.TeleBot(token, parse_mode=None)
+
+
+    @bot.message_handler(func=lambda m: True, content_types=['new_chat_participant'])
+    def on_user_joins(message):
+        user = message.from_user
+        if hasattr(user, 'first_name') and user.first_name is not None:
+            name = user.first_name
+            if hasattr(user, 'last_name') and user.last_name is not None:
+                name = f'{name} {user.last_name}'
+        else:
+            name = 'Rockstar'
+
+        bot.reply_to(message, messages['welcome'].format(name=name))
+
+
+    @bot.message_handler(commands=['reglas'])
+    def rules(message):
+        bot.reply_to(message, messages['rules'])
+
+
+    @bot.message_handler(commands=['proyectos'])
+    def projects(message):
+        bot.reply_to(message, messages['projects'])
+        for project in messages['current_projects']:
+            bot.reply_to(message, project)
+
+
+    @bot.message_handler(commands=['donaciones'])
+    def donations(message):
+        bot.reply_to(message, messages['donations'])
+
+
+    bot.polling()
 else:
-    print('Please, ensure to provide a token using the enviroment variables.')
-
-@bot.message_handler(func=lambda m: True, content_types=['new_chat_members'])
-def on_user_joins(message):
-    user_name = message.new_chat_participants.first_name
-    print(user_name)
-
-@bot.message_handler(commands=['help'])
-def send_help_message(message):
-    import pdb; pdb.set_trace()
-    bot.reply_to(message, 'Hi , you need help?')
-
-
-bot.polling()
+    print('[!] Please, ensure to provide a the token using the enviroment variables.')
---
+++ message.py
@@ -0,0 +1,46 @@
+messages = {
+    'welcome':
+        u'Bienvenido a La Comunidad HackTzi, {name}!\n'
+        u'¿Cuál carrera de tech estás haciendo y de dónde vienes? 😁🙌🏻'
+        u'Te invlitamos a consular las reglas y proyectos usando /reglas /proyectos',
+    'rules':
+        u'Reglas\n'
+        u'▪️Respetar a sus comapañeros\n'
+        u'▪️No hablar de temas que vayas más allá del mundo tech\n'
+        u'▪️No aceptamos acoso de ningún tipo\n\n'
+        u'🚨 Si incumple con alguna de las reglas será baneado del grupo',
+    'projects':
+        u'Si estás interesado en hacer algún proyecto puedes encontrar toda'
+        u'la información al respecto aquí 👇 \n\n'
+        u'http://notion.so/Proyectos-Doc-HackTzi-d1e9d4d49d3f4cb7a8c88452ddecc383',
+    'current_projects': [
+        u'🏬 Proyecto Tienda Platzi\n\n'
+        u'Canal de discord: #p2-blog-hacktzi\n\n'
+        u'Tecnologías: React.js, Wordpress, Fontity, Python, Kotlin, PHP, copywriting.'
+        u'Descripción:\n\n'
+        u'Un sitio web tipo Blog para la comunidad de Platzi pueda compartir apuntes, '
+        u'docs de proyectos y ayudas de código.\n\n'
+        u'',
+        u'📚 Proyecto Blog HackTzi\n\n'
+        u'Canal de discord: #p1-tienda-platzi\n\n'
+        u'Tecnologías: React.js, Python, Sass, Node.js'
+        u'Descripción:\n\n'
+        u'Un sitio web tipo Blog para la comunidad de Platzi pueda compartir apuntes,'
+        u'docs de proyectos y ayudas de código.\n\n'
+        u'El sitio web va a ser hecho con Wordpress en el lado del backend, y con Frontity'
+        u'(React Framework) del lado del Front-end. Además tenemos un mini proyecto con'
+        u' Inteligencia artificial con Python al repositorio de WaveNet.\n\n'
+        u'También, hacer una versión beta de blog en una App móvil con Kotlin.',
+        u'💿 Proyecto App Desktop Platzi\n\n'
+        u'Canal de discord: #p3-app-desktop-platzi\n\n'
+        u'Tecnologías: Angular, Python, Javascript, Sass, eslint, Electron'
+        u'Descripción:\n\n'
+        u'App desktop de Platzi que sea multiplataforma, funcione en Linux, MACOS y Windows.'
+        u'El público objetivo son los estudiantes de Platzi, que no tengan que abrir varias'
+        u'pestañas para poder ver sus clases todo desde un aplicación escritorio.',
+    ],
+    'donations':
+        u'Si quieres apoyar a la comunidad de forma monetaria, puedes donar por aquí:\n\n'
+        u'Este método no está disponible para Latam\n'
+        u'https://paypal.me/pools/c/8wq4DXcLDI'
+}
