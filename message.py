25266726958c44097755d9adbe2b27592878a5a8
Royer G <royjuni3431@gmail.com>
2021-01-29 08:23:17 -0500

    refactor: improve messages and correct grammar

--- message.py
+++ message.py
@@ -1,7 +1,7 @@
 messages = {
     'welcome':
         u'Bienvenido a La Comunidad HackTzi, {name}!\n'
-        u'¿Cuál carrera de tech estás haciendo y de dónde vienes? 😁🙌🏻'
+        u'¿Cuál carrera de tech estás haciendo y de dónde vienes? 😁🙌🏻\n\n'
         u'Te invitamos a consular las reglas y proyectos usando /reglas /proyectos',
     'rules':
         u'Reglas\n'
@@ -10,33 +10,32 @@ messages = {
         u'▪️No aceptamos acoso de ningún tipo\n\n'
         u'🚨 Si incumple con alguna de las reglas será baneado del grupo',
     'projects':
-        u'Si estás interesado en hacer algún proyecto puedes encontrar toda'
+        u'Si estás interesado en hacer algún proyecto puedes encontrar toda '
         u'la información al respecto aquí 👇 \n\n'
         u'http://notion.so/Proyectos-Doc-HackTzi-d1e9d4d49d3f4cb7a8c88452ddecc383',
     'current_projects': [
-        u'🏬 Proyecto Tienda Platzi\n\n'
-        u'Canal de discord: #p2-blog-hacktzi\n\n'
-        u'Tecnologías: React.js, Wordpress, Fontity, Python, Kotlin, PHP, copywriting.'
-        u'Descripción:\n\n'
-        u'Un sitio web tipo Blog para la comunidad de Platzi pueda compartir apuntes, '
-        u'docs de proyectos y ayudas de código.\n\n'
-        u'',
-        u'📚 Proyecto Blog HackTzi\n\n'
-        u'Canal de discord: #p1-tienda-platzi\n\n'
-        u'Tecnologías: React.js, Python, Sass, Node.js'
-        u'Descripción:\n\n'
+        u'🏬 Proyecto Tienda Platzi\n'
+        u'Canal de discord: #p1-tienda-platzi\n'
+        u'Tecnologías:  React.js, Python, Sass, Node.js\n\n'
+        u'Descripción:\n'
+        u'Una tienda con productos de Platzi donde el usuario pueda tener una experiencia '
+        u'satisfactoria, además de compartir su review del producto y publicar una foto con este.',
+        u'📚 Proyecto Blog HackTzi\n'
+        u'Canal de discord: #p2-blog-hacktzi\n'
+        u'Tecnologías: React.js, Wordpress, Fontity, Python, Kotlin, PHP, copywriting.\n\n'
+        u'Descripción:\n'
         u'Un sitio web tipo Blog para la comunidad de Platzi pueda compartir apuntes,'
         u'docs de proyectos y ayudas de código.\n\n'
         u'El sitio web va a ser hecho con Wordpress en el lado del backend, y con Frontity'
-        u'(React Framework) del lado del Front-end. Además tenemos un mini proyecto con'
+        u'(React Framework) del lado del Front-end. Además tenemos un mini proyecto con '
         u' Inteligencia artificial con Python al repositorio de WaveNet.\n\n'
         u'También, hacer una versión beta de blog en una App móvil con Kotlin.',
-        u'💿 Proyecto App Desktop Platzi\n\n'
-        u'Canal de discord: #p3-app-desktop-platzi\n\n'
-        u'Tecnologías: Angular, Python, Javascript, Sass, eslint, Electron'
-        u'Descripción:\n\n'
-        u'App desktop de Platzi que sea multiplataforma, funcione en Linux, MACOS y Windows.'
-        u'El público objetivo son los estudiantes de Platzi, que no tengan que abrir varias'
+        u'💿 Proyecto App Desktop Platzi\n'
+        u'Canal de discord: #p3-app-desktop-platzi\n'
+        u'Tecnologías: Angular, Python, Javascript, Sass, eslint, Electron\n\n'
+        u'Descripción:\n'
+        u'App desktop de Platzi que sea multiplataforma, funcione en Linux, MACOS y Windows. '
+        u'El público objetivo son los estudiantes de Platzi, que no tengan que abrir varias '
         u'pestañas para poder ver sus clases todo desde un aplicación escritorio.',
     ],
     'donations':
