1fefed558df01e8caf106a618fcd76f5511d4bb9
Royer G <royjuni3431@gmail.com>
2021-04-10 17:48:24 -0500

    solve bugs

--- message.py
+++ message.py
@@ -5,7 +5,7 @@ messages = {
         u'Bienvenido a La Comunidad HackTzi, {name}!\n'
         u'¿Cuál carrera de tech estás haciendo y de dónde vienes? 😁🙌🏻\n\n'
         u'Te invitamos a consular las reglas y proyectos usando /reglas /proyectos\n\n'
-        u'Ingresa el server de discord de HackTzi: {}'.format(DISCORD_LINK),
+        u'Ingresa el server de discord de HackTzi: {discord}'.format(discord=DISCORD_LINK),
     'rules':
         u'Reglas\n'
         u'▪️Respetar a sus comapañeros\n'
@@ -46,7 +46,7 @@ messages = {
         u'Este método no está disponible para Latam\n'
         u'https://paypal.me/pools/c/8wq4DXcLDI',
     'discord':
-        u'Ingresa el server de discord de HackTzi: {}'.format(DISCORD_LINK),
+        u'Ingresa el server de discord de HackTzi: {discord}'.format(discord=DISCORD_LINK),
     'bug':
         u'418 I\'m a teapot',
         }
